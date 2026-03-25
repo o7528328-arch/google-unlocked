228d94b23aafe1d8495d89c247baa02ba979ec5f
BIGG_BUBBA <o7528328@gmail.com>
2026-03-25 03:06:25 +0200

    i am trying to make this work with manifest v3 , i am aware that this is shit put it works

--- extension/u.js
+++ extension/u.js
@@ -1,57 +1,49 @@
 $(function () {
-    if (window.location.href.indexOf('//www.google') === -1) {
-        return
+    console.log("Google Unlocked: Active");
+
+    // Look for the notice links
+    const notices = $('a').filter(function() {
+        const href = $(this).attr('href') || '';
+        return href.includes('lumendatabase.org/notices/') || href.includes('chillingeffects.org/notice.cgi');
+    });
+
+    if (notices.length === 0) return;
+
+    // Create results box
+    if ($('#unlocked-box').length === 0) {
+        $('#rcnt').append(`
+            <div id="unlocked-box" style="margin:20px; padding:15px; border:1px solid #dadce0; border-radius:8px; background:#f8f9fa;">
+                <h3 style="color:#1a0dab; margin-top:0;">🔓 Unlocked Results</h3>
+                <div id="unlocked-content">Loading hidden links...</div>
+            </div>
+        `);
     }
-    $('#search div.g').last().after('<div id="cc"></div>')
-    var s = $('#cc')
 
-    $('div i > a').each(function (i, a) {
-        if (a.href === 'https://www.google.com/support/answer/1386831') return;
-        setTimeout(function () {
-            $.ajax({
-                type: 'GET',
-                url: a.href,
-                dataType: 'html',
-                success: function (data) {
-                    var hm = {}
-                    var links = data.matchAll(/class="infringing_url">([^\s-<]+)\s*-\s*([0-9]+)/g)
-                    for (const i of links) {
-                        if (i[1] in hm) {
-                            continue
-                        }
-                        hm[i[1]] = 1
-                        var l = $('#l' + i[2])
-                        if (l.length < 1) {
-                            s.prepend('<div id="l' + i[2] + '" data-num="' + i[2] + '"></div>')
-                            l = $('#l' + i[2])
-                        }
-                        l.append('<div class="g">'
-                            + '<a href="http://' + i[1] + '" target="_blank">' + i[1] + ' (' + i[2] + ' URLs) </a>'
-                            + '</div>')
-                    }
-                    var divs = $('div[data-num]', s)
-                    divs.sort(function (a, b) {
-                        return b.dataset.num - a.dataset.num
-                    })
-                    s.html(divs)
-                },
-                error: function (e, err) {
-                    console.log(e, err);
-                },
-                xhr: function () {
-                    var xhr = jQuery.ajaxSettings.xhr();
-                    var setRequestHeader = xhr.setRequestHeader;
-                    xhr.setRequestHeader = function (name, value) {
-                        if (name == 'X-Requested-With') return;
-                        setRequestHeader.call(this, name, value);
-                    }
-                    return xhr;
-                },
+    notices.each(function (i, el) {
+        const href = $(el).attr('href');
+        const idMatch = href.match(/sID=(\d+)/) || href.match(/notices\/(\d+)/);
+        if (!idMatch) return;
 
-            });
+        // DIRECT URL (no www) to avoid the redirect loop
+        const targetUrl = `https://lumendatabase.org/notices/${idMatch[1]}`;
+
+        setTimeout(() => {
+            $.get(targetUrl, function(data) {
+                // Regex to find domains and URL counts
+                const regex = /class="infringing_url">([^<]+)\s*-\s*(\d+)/g;
+                let match;
+                let htmlOutput = "";
+
+                while ((match = regex.exec(data)) !== null) {
+                    htmlOutput += `<div style="margin:5px 0;">
+                        <a href="http://${match[1].trim()}" target="_blank" style="text-decoration:none; font-weight:bold;">${match[1].trim()}</a> 
+                        <span style="color:#70757a;">(${match[2]} links)</span>
+                    </div>`;
+                }
+                
+                if (i === 0) $('#unlocked-content').empty();
+                $('#unlocked-content').append(htmlOutput);
+            }).fail((err) => console.log("Lumen Fetch Error:", err));
         }, i * 2000);
-    })
-
-
+    });
 });
-
